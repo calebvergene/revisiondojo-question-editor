c710e34fbf4a7e81a5893a9d849ef8210240d148
caleb vergene <calebverg@gmail.com>
2025-04-11 20:53:37 -0700

    feat: question specification form

--- src/components/question-editor/QuestionModal.tsx
+++ src/components/question-editor/QuestionModal.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { useState } from "react";
 import {
     Dialog,
     DialogContent,
@@ -6,19 +8,44 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import { Textarea } from "@/components/ui/textarea"
+import { Label } from "@/components/ui/label"
+
 
 
 const QuestionModal = () => {
+    const [question, setQuestion] = useState({
+        id: crypto.randomUUID(),
+        specification: '',
+        options: [],
+        parts: []
+    });
+
+
+    // For specification
+    const [specification, setSpecification] = useState("");
+    const handleMessageChange = (e: { target: { value: string; }; }) => {
+        setSpecification(e.target.value);
+    };
+
+
     return (
         <Dialog>
             <DialogTrigger>Open</DialogTrigger>
             <DialogContent>
                 <DialogHeader>
-                    <DialogTitle>Are you absolutely sure?</DialogTitle>
-                    <DialogDescription>
-                        This action cannot be undone. This will permanently delete your account
-                        and remove your data from our servers.
-                    </DialogDescription>
+                    <DialogTitle>Create a Question</DialogTitle>
+                    <form onSubmit={() => console.log('eventually...')}>
+                        <div className="grid w-full gap-1.5 mt-4">
+                            <Label htmlFor="message">Question</Label>
+                            <Textarea
+                                placeholder="Type your question here."
+                                id="message"
+                                value={specification}
+                                onChange={handleMessageChange}
+                            />
+                        </div>
+                    </form>
                 </DialogHeader>
             </DialogContent>
         </Dialog>
